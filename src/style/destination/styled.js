ff3f2d5e7ef571de53ffb92284b65fb143a6b9da
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2023-01-06 18:23:16 +0100

    styles for tablets&desktop

--- src/style/destination/styled.js
+++ src/style/destination/styled.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import DDesktop from "../../assets/destination/background-destination-desktop.jpg";
 import DTablet from "../../assets/destination/background-destination-tablet.jpg";
+import DMobile from "../../assets/destination/background-destination-mobile.jpg";
 import { NavLink } from "react-router-dom";
+
 
 export const Container = styled.main`
     background-image: url("${DMobile}");
@@ -18,11 +20,11 @@ export const Container = styled.main`
     @media (min-width: 768px) {
         background-image: url(${DTablet});
       }
-    @media (min-width: 1028px) {
+    @media (min-width: 1280px) {
         background-image: url(${DDesktop});
-        flex-direction: row;
+        flex-direction: column;
         justify-content: space-evenly;
-        min-height: 760px;
+        align-items: center;
     }
 `;
 
@@ -38,20 +40,21 @@ export const Header = styled.div`
         font-size: 20px;
         font-weight: 400;
         line-height: 24px;
-        letter-spacing: 3.375px;
+        letter-spacing: 3.38px;
+        margin: 0 0 0 -400px;
     }
 
-    @media(min-width: 1028px) {
+    @media(min-width: 1280px) {
         font-weight: 400;
         font-size: 28px;
         letter-spacing: 4.725px;
+        margin: 0 0 0 -700px;
     }
 `;
 
 export const Span = styled.span`
     padding-right: 15px;
     opacity: 0.25;
-    font-size: 20px;
     font-weight: 700;
     line-height: 19px;
     letter-spacing: 2.7px; 
@@ -59,10 +62,10 @@ export const Span = styled.span`
     @media (min-width: 768px) {
         font-size: 20px;
         line-height: 24px;
-        letter-spacing: 3.375px;
+        letter-spacing: 3.38px;
     }
 
-    @media(min-width: 1028px) {
+    @media(min-width: 1280px) {
         font-size: 28px;
         letter-spacing: 4.725px;
     }
@@ -74,6 +77,14 @@ export const Section = styled.article`
     align-items: center;
     justify-content: center;
     padding: 29px 0 58px 0;
+
+     @media(min-width: 1280px) {
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        width: 1200px;
+        padding: 0;
+    }
 `;
 
 export const Image = styled.img`
@@ -84,7 +95,12 @@ export const Image = styled.img`
     @media (min-width: 768px) {
         width: 300px;
         height: 300px;
-        margin-bottom: 53px;
+        margin: 50px 0 53px 0;
+    }
+
+    @media (min-width: 1280px) {
+        width: 445px;
+        height: 445px;
     }
 `;
 
@@ -94,6 +110,15 @@ export const Tab = styled.div`
     align-items: center;
     justify-content: center;
     text-align: center;
+
+     @media (min-width: 768px) {
+        width: 575px;
+    }
+
+    @media (min-width: 1280px){
+        width: 445px;
+        align-items: flex-start;
+    }
 `;
 
 export const DestinationNav = styled.nav`
@@ -160,6 +185,15 @@ export const Planet = styled.section`
     flex-direction: column;
     align-items:center;
     justify-content: center;
+
+     @media (min-width: 768px) {
+        width: 575px;
+    }
+
+    @media (min-width: 1280px) {
+        width: 475px;
+        align-items: flex-start;
+    }
 `;
 
 export const Title = styled.p`
@@ -171,6 +205,16 @@ export const Title = styled.p`
     text-align: center;
     color: white;
     margin: 0;
+
+     @media (min-width: 768px) {
+        font-size: 80px;
+        line-height: 91.68px;
+    }
+
+    @media (min-width: 1280px) {
+        font-size: 100px;
+        line-height: 115px;
+    }
 `;
 
 export const Info = styled.p`
@@ -184,6 +228,21 @@ export const Info = styled.p`
     width: 327px;
     margin: 0 0 20px 0;
     height: 125px;
+
+     @media (min-width: 768px) {
+        font-size: 16px;
+        line-height: 28px;
+        width: 575px;
+        height: 84px;
+    }
+
+    @media (min-width: 1280px) {
+        font-size: 18px;
+        line-height: 32px;
+        width: 450px;
+        margin: 0 0 50px 0;
+        text-align: left;
+    }
 `;
 
 export const Data = styled.section`
@@ -191,13 +250,37 @@ export const Data = styled.section`
     flex-direction: column; 
     border-top: 1px solid #383B4B;
     margin: 20px 24px 0 24px;
-    width: 80% 
+    width: 80%;
+
+    @media (min-width: 768px){
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+    }
+
+    @media (min-width: 1280px) {
+        width: 100%;
+        margin-left: 0;
+        align-items: flex-start;
+        justify-content: flex-start;
+    }
 `;
 
 export const DataInfo = styled.div`
     margin: 32px 0 0 0;
     padding:0;
-    text-align: center;  
+    text-align: center;
+    
+    @media (min-width: 768px) {
+        margin: 28px 0 0 0;
+    }
+
+    @media (min-width: 1280px) {
+        text-align: left;
+        &:nth-child(2) {
+            margin-left: 79px;
+        }
+    }
 `;
 
 export const DataP = styled.p`
@@ -206,9 +289,9 @@ export const DataP = styled.p`
     font-weight: 400;
     font-size: 14px;
     line-height: 17px;
-    letter-spacing: 2.3625px;
+    letter-spacing: 2.36px;
     text-transform: uppercase;
-    color: #D0D6F9; 
+    color: #D0D6F9;
 `;
 
 export const DataSpan = styled.span`
